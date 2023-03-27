cab4adba73fb2a8e37421cb44da7cfb5f6cac15a
Parker <parkerlamarbrook@gmail.com>
2023-03-26 21:38:04 -0600

    API calls to get current weather and 5 day forecast are working and return their data sets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,92 @@
-let city;
-let state;
-let country;
-
 var fetchButton = document.getElementById('fetch-button');
-
 let APIKey = "fe8c1985df685a70d00e6d5098ec6a2d";
-let weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-// let geoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=10&appid=" + APIKey;
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-// let byCityURL = "api.openweathermap.org/data/2.5/weather?q=city&appid=APIKey"
-let queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 
+$("#fetch-weather-button").on("click", function getCurrentWeather() {
+    let city = $(this).siblings("#city-submit").val();
+    let geoCodeURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-
-
-
-city = "Salt Lake City";
-state = "UT";
-country = "US"
-
-function getGeoAPI(city, state, country, APIKey){
-    city = "Salt Lake City";
-    state = "UT";
-    country = "US";
-    let geoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=10&appid=" + APIKey;
-        fetch(geoCodeURL)
-            .then(function (response){
+    fetch(geoCodeURL)
+        .then(function (response){
             return response.json();
         })
-        .then(function (data) {
-            console.log(data);
-            let lat = data[0].lat;
-            let lon = data[0].lon;
-            console.log(lat,lon)
-            return lat, lon;
+        .then(function (weatherData) {  
+            console.log('Current Weather');   
+            console.log(weatherData);       
+            let lat = weatherData.coord.lat;
+            let lon = weatherData.coord.lon;
+            console.log("Latitude: " + lat + "   ||  Longitude: " + lon);
+        
+            let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial";
+            // console.log("Weather URL: " + queryWeatherURL);
+            
+            fetch(forecastURL)
+                .then(function (response){
+                    return response.json();
+                })
+                .then(function (forecastData) {
+                    console.log('Five Day Forecast:');
+                    console.log(forecastData);
+                })
         })
-}
+})
 
-function getWeatherAPI(lat, lon, APIKey){
-    let queryWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
-    fetch(queryWeatherURL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
-}
 
-fetchButton.addEventListener('click', getGeoAPI);
+
+
+
+
+
+
+// function getGeoAPI(city){
+//     city = "Salt Lake City";
+//     state = "UT";
+//     country = "US";
+//     // let geoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=10&appid=" + APIKey;
+//     let geoCodeURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+//     fetch(geoCodeURL)
+//         .then(function (response){
+//             return response.json();
+//         })
+//         .then(function (data) {  
+//             console.log('Fetch Response: \n-------------');   
+//             console.log(data);       
+//             let lat = data.coord.lat;
+//             let lon = data.coord.lon;
+//             console.log("Latitude: " + lat + "   ||  Longitude: " + lon);
+        
+//             let queryWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial";
+//             console.log("Weather URL: " + queryWeatherURL);
+            
+//             fetch(queryWeatherURL)
+//                 .then(function (response){
+//                     return response.json();
+//                 })
+//                 .then(function (weatherData) {
+//                     console.log('Fetch Response: \n-------------');
+//                     console.log(weatherData);
+//                 })
+//         })
+// }
+
+// function getWeatherAPI(lat, lon){
+//     let queryWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+//     console.log(queryWeatherURL);
+//     fetch(queryWeatherURL)
+//     .then(function (response){
+//         return response.json();
+//     })
+//     .then(function (datas) {
+//         console.log('Fetch Response \n-------------');
+//         console.log(datas);
+//     })
+// }
+
+// function getWeather(){
+//     getGeoAPI();
+//     // getWeatherAPI();
+// }
+
+
+
