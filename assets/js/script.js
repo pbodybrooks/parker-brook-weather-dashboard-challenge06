65589b584316ce48602b6930633b838e03c0fdf8
Parker <parkerlamarbrook@gmail.com>
2023-03-27 19:35:53 -0600

    fixed search issue by preventing default behaviorin the event listener function. added limiter on cityList size and reversed the order.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,72 @@
+// API key:
 const APIKey = "fe8c1985df685a70d00e6d5098ec6a2d";
-// const fetchButton = document.getElementById('fetch-button');
+
+// Global Selectors
 const input = document.querySelector('#city-input');
 const button = document.querySelector('#save-button');
 const cityList = document.querySelector('#city-list');
-// let city = document.querySelector('#city-input').value;
+const cityDisplayMax = 15;
 
 
-$("#save-button").on("click", getWeather);
-$("#save-button").on("click", saveCity);
-
-// $("#save-button").on("click", function () {
-//     getWeather();
-//     saveCity();
-// })
-
-function getWeather(city) {
-    console.log("City is: " + city);
-    if (city === undefined){
-        // let city = $(this).siblings("#city-input").val();
-        let city = document.querySelector('#city-input').value
+// this listens for the enter key rather than a click because its more intuitive
+$("#city-input").keydown(function(event) {
+    if (event.keyCode === 13) {
+        // prevent default is needed because the save button will otherwise try to take in "city" as an object
+        event.preventDefault();
+        // if keydown detected, click save button
+        $("#save-button").click();
     }
+});
 
-  let weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// this listens for a click on the Get Weather! button 
+$("#save-button").on("click", function (event) {
+    event.preventDefault();
+    // set city to the value entered into the input
+    const city = $("#city-input").val();
+    // get the weather, feed it city
+    getWeather(city);
+    // save the city to local storage
+    saveCity();
+})
 
-  fetch(weatherURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (weatherData) {
-      console.log('Current Weather');
-      console.log(weatherData);
-      let lat = weatherData.coord.lat;
-      let lon = weatherData.coord.lon;
-      console.log("Latitude: " + lat + "   ||  Longitude: " + lon);
-      getForecast(lat, lon);
-    });
+// getWeather does two things when fed a city: it gets the weather, and stores the latitude and longitude in variables
+function getWeather(city) {
+    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    console.log("Weather URL: " + weatherURL);
+    // fetch URL
+    fetch(weatherURL)
+        .then(function (response) {
+        // get JSON format response
+        return response.json();
+        })
+        // get weather data
+        .then(function (weatherData) {
+        console.log('Current Weather in ' + city);
+        console.log(weatherData);
+        // store latitude and longitude values for getForecast
+        let lat = weatherData.coord.lat;
+        let lon = weatherData.coord.lon;
+        console.log("Latitude: " + lat + "   ||  Longitude: " + lon);
+        // call getForecast and feed it lat, lon, and city (not required)
+        getForecast(lat, lon, city);
+        });
 }
 
-function getForecast(lat, lon) {
-  let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial";
-  fetch(forecastURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (forecastData) {
-      console.log('Five Day Forecast:');
-      console.log(forecastData);
-    });
+// get forecast takes in latitude and longitude (And city simply for console log) to return 5-day forecast data
+function getForecast(lat, lon, city) {
+    let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial";
+    console.log("Forecast URL: " + forecastURL);
+    fetch(forecastURL)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (forecastData) {
+        console.log('Five Day Forecast in ' + city);
+        console.log(forecastData);
+        });
 }
 
+// saveCity saves the city to local storage if it is not already there
 function saveCity () {
     // get the current list of cities from local storage
     const cities = JSON.parse(localStorage.getItem('cities')) || [];
@@ -56,9 +74,13 @@ function saveCity () {
     const cityName = toTitleCase(input.value.trim());
     // check if the city name already exists in the stored list of cities
     const cityExists = cities.includes(cityName);
-    // ff the city doesn't exist, add it to the list and save it to local storage
+    // if the city doesn't exist, add it to the list and save it to local storage
     if (!cityExists) {
       cities.push(cityName);
+      // remove cities beyond cityDisplayMax value
+      if (cities.length >= cityDisplayMax) {
+        cities.shift();
+        }
       localStorage.setItem('cities', JSON.stringify(cities));
     }
     // reset the input field to an empty string
@@ -69,39 +91,34 @@ function saveCity () {
 
 // display the list of cities as clickable search buttons
 function displayCities(cities) {
+    // reverse cityList so that most recent searches are at the top
+  cities = cities.reverse();  
   cityList.innerHTML = '';
+  // for each city, create a button with class "city-button" and an event listener to run getWeather for that city
   cities.forEach(cityName => {
     const button = document.createElement('button');
     button.classList.add("city-button");
+    // create a var cityName out of the buttons text content
     button.textContent = cityName;
     button.addEventListener('click', () => {
-      getWeather(button.textContent);
+        // give getWeather the city name as an argument 
+        getWeather(cityName);
     });
     cityList.appendChild(button);
   });
 }
 
-cityList.addEventListener("click", function () {
-    const city = input
-})
-
-// if (button.classList.contains('city-button')){
-//     button.addEventListener("click", getWeather(button.innerText));
-// }
-
-
-// display saved city searches
+// display saved city searches at all times
 const cities = JSON.parse(localStorage.getItem('cities')) || [];
 displayCities(cities);
 
-// clear local storage, removing all saved city searches
+// allows user clear local storage, removing all saved city searches
 $("#clear-cities").on("click", function() {
     localStorage.clear();
     location.reload();
 })
 
-// this function just changes whatever the user's input was to title case so it looks nicer
-// the original code I wrote only changed the first word's to uppercase rather than every word (ex. Salt lake city)
+// this function just changes whatever the user's input was to title case so it looks nicer when stored
 function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt){
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
